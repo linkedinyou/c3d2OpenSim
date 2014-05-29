0a0f0c5a1e247264659aedf494c844ebca61882b
james Dunne <james.dunne@stanford.edu>
2014-05-29 16:17:00 -0700

    Long trial edits
    Tiny errors in the forceplate calculations can add up in very long
    trials and can cause the estimates of force to be non-zero. Added some
    changes in the code that takes into account the length of the trial by
    trying to reduce the cumulative effects of noise.

--- c3dExport2Opensim/forces2Global.m
+++ c3dExport2Opensim/forces2Global.m
@@ -13,7 +13,8 @@ function structData = forces2Global(structData)
 % system for calculating COP. 
 
 for i = 1 : length(structData.fp_data.GRF_data)
-
+    
+    forceColm = [1:3];
     forceNames = fieldnames(structData.fp_data.FP_data(i).channels);
     
     for u = 1:3
@@ -28,58 +29,56 @@ for i = 1 : length(structData.fp_data.GRF_data)
     [maxValue globalVgrfCol] = max(max(abs(structData.fp_data.GRF_data(i).F)));
     % get the difference between the two vertical ground reaction forces
     zDiff = structData.fp_data.GRF_data(i).F(:,globalVgrfCol) - forces(:,vgrfCol);
-    
+    % get the abs max of that difference
+    [diffMax colN] = max(abs(zDiff));
     % if they are in the same coordinate system then they would negate
-    % one another and be less than the max. If greater than the max, then
-    % the force needs to be flipped. 
-    if max(zDiff) > max(forces(:,vgrfCol))
+    % one another and be 0. If not then sign needs to be flipped. 
+    if sign(zDiff(colN)) ~= 0
         forces(:,vgrfCol) = -forces(:,vgrfCol);
         moments(:,vgrfCol) = -moments(:,vgrfCol);
-    end
-
+    end 
+    
+    forceColm(vgrfCol) = [];
+    
     % before we do the same to fx and fy, we need to check if they are 
     % both in the correct frame. If the forceplates have been rotated in
     % the global the forceplate x will be in the global y and vice versa.
 
     % get abs max of the x force from the channels
-    xMaxFP = max(abs(forces(:,1)));
+    xMaxFP = max(abs(forces(:,forceColm(1))));
 
     % get abs max of the global forces
-    xMaxGlobal =   max(abs(structData.fp_data.GRF_data(i).F(:,1)));
-    yMaxGlobal =   max(abs(structData.fp_data.GRF_data(i).F(:,2)));
+    xMaxGlobal =   max(abs(structData.fp_data.GRF_data(i).F(:,forceColm(1))));
+    yMaxGlobal =   max(abs(structData.fp_data.GRF_data(i).F(:,forceColm(2))));
 
-    if xMaxFP == yMaxGlobal
+    if round(xMaxFP) == round(yMaxGlobal)
        % reoder colomns so that the forceplate X is now in the Y col.
-       forces = [forces(:,2) forces(:,1) forces(:,3)];
-       moments = [moments(:,2) moments(:,1) moments(:,3)]; 
+       forces = [forces(:,forceColm(2)) forces(:,forceColm(1)) forces(:,vgrfCol)];
+       moments = [moments(:,forceColm(2)) moments(:,forceColm(1)) moments(:,vgrfCol)]; 
     end
     
-
     % get the differences between the x and y component forces
-    xDiff = structData.fp_data.GRF_data(i).F(:,1) - forces(:,1);
-    yDiff = structData.fp_data.GRF_data(i).F(:,2) - forces(:,2);
+    xDiff = structData.fp_data.GRF_data(i).F(:,forceColm(1)) - forces(:,forceColm(1));
+    yDiff = structData.fp_data.GRF_data(i).F(:,forceColm(2)) - forces(:,forceColm(2));
 
     % if the columns are the same their diff will = 0. Otherwise, flip 
-    if  sum(structData.fp_data.GRF_data(i).F(:,1) - forces(:,1)) ~= 0 
-        forces(:,1) = -forces(:,1);
-        moments(:,1) = -moments(:,1);
+    if  mean(round((structData.fp_data.GRF_data(i).F(:,forceColm(1)) - forces(:,forceColm(1))))) ~= 0 
+        forces(:,1) = -forces(:,forceColm(1));
+        moments(:,1) = -moments(:,forceColm(1));
     end
     
     % if the columns are the same their diff will = 0. Otherwise, flip 
-    if sum(structData.fp_data.GRF_data(i).F(:,2) - forces(:,2)) ~= 0 
-        forces(:,2)  = -forces(:,2);
-        moments(:,2) = -moments(:,2);
+    if mean(round((structData.fp_data.GRF_data(i).F(:,forceColm(2)) - forces(:,forceColm(2)))))~= 0 
+        forces(:,forceColm(2))  = -forces(:,forceColm(2));
+        moments(:,forceColm(2)) = -moments(:,forceColm(2));
     end
 
        
 %     
-     hold on
-     plot(forces, 'r')
-     plot(structData.fp_data.GRF_data(i).F, 'k')
+     %hold on
+     %plot(forces, 'r')
+     %plot(structData.fp_data.GRF_data(i).F, 'k')
      
-     hold on
-     plot(forces(:,1), 'r')
-     plot(structData.fp_data.GRF_data(i).F(:,2), 'k')
      
 %     
     structData.fp_data.GRF_data(i).F_original = structData.fp_data.GRF_data(i).F;
